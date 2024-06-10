d9361773c4f5d30c5723e27405fe0e462929cb27
achimugu <sachiimugu@gmail.com>
2024-06-10 17:01:30 +0100

    s

--- components/ContactLogos.jsx
+++ components/ContactLogos.jsx
@@ -10,10 +10,10 @@ const ContactLogos = () => {
         </p>
     <div className='flex gap-8 md:gap-16 justify-center item-center px-4 py-2 md:p-8 bg-sky-200 shadow-md '>
         
-        <Image src='/images/gm.svg' width={90} height={90} alt='icons' />
-        <Image src='/images/wh.svg' width={100} height={100} alt='icons' />
-        <Image src='/images/te.svg' width={100} height={100} alt='icons' />
-        <Image src='/images/li.svg' width={100} height={100} alt='icons' />
+        <Image src='/images/gm.svg' width={90} height={90} alt='icons' className="cursor-pointer" />
+        <Image src='/images/wh.svg' width={100} height={100} alt='icons' className="cursor-pointer"/>
+        <Image src='/images/te.svg' width={100} height={100} alt='icons' className="cursor-pointer" />
+        <Image src='/images/li.svg' width={100} height={100} alt='icons' className="cursor-pointer" />
     </div>
     </div>
   )
