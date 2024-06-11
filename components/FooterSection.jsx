a35a15e6d5a90700bbd214452e13978cf7fc15aa
achimugu <sachiimugu@gmail.com>
2024-06-11 17:52:16 +0100

    s

--- components/FooterSection.jsx
+++ components/FooterSection.jsx
@@ -21,34 +21,34 @@ function FooterSection() {
           </p>
         </div>
         <div className="flex gap-8 flex-grow justify-around">
-          <div className="flex flex-col justify-center  items-center gap-4">
-            <p className="text-slate-200 font-[700] text-[1.3125rem]">
+          <div className="flex flex-col justify-center  items-center gap-2">
+            <p className="text-slate-200 font-[700] text-lg md:text-[1.3125rem]">
               Company
             </p>
             <div className="flex flex- gap-2">
-              <p className="text-slate-400 text-[1.125rem] hover:text-orange-500 font-[500]">
+              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 md:font-[500]">
                 About
               </p>
-              <p className="text-slate-400 text-[1.125rem] hover:text-orange-500 font-[500]">
+              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 md:font-[500]">
                 Careers
               </p>
-              <p className="text-slate-400 text-[1.125rem] hover:text-orange-500 font-[500]">
+              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 md:font-[500]">
                 Mobile
               </p>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col justify-center items-center gap-2">
             <p className="text-slate-200 font-[700]  text-[1.3125rem]">
               Contact
             </p>
             <div className="flex flex- gap-2">
-              <p className="text-slate-400 text-[1.125rem] hover:text-orange-500 font-[500]">
+              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
                 Help
               </p>
-              <p className="text-slate-400 text-[1.125rem] hover:text-orange-500 font-[500]">
+              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
                 Press
               </p>
-              <p className="text-slate-400 text-[1.125rem] hover:text-orange-500 font-[500]">
+              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
                 Affilates
               </p>
             </div>
