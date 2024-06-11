c721e9579964a9cdff05d7ca8074a28ca28ba89b
achimugu <sachiimugu@gmail.com>
2024-06-11 12:19:31 +0100

    s

--- components/Heroo.jsx
+++ components/Heroo.jsx
@@ -6,7 +6,7 @@ const Heroo = () => {
     <div className="lg:min-h-screen min-h-screen pb-5 bg-slate-900">
       <div className="max-w-[100rem] mx-auto p-2 md:p-16">
         <div className="mt-36 sm:mt-0">
-          <p className="text-3xl md:text-6xl font-extrabold text-center p-4 md:px-16 md:py-6 text-white">
+          <p className="text-3xl md:text-6xl font-extrabold text-center p-4 md:px-16 md:py-10 text-white">
             {/* Remote Rater for Data Validation and Relevance Jobs
              */}
             Raters for Data Validation and Search Relevance Evaluation
@@ -14,12 +14,12 @@ const Heroo = () => {
           <p className="text-sm md:text-2xl  text-center p-4 leading-6 md:px-16 text-white">
             In a world where real income is fast shrinking, <span className="text-sky-500 text-sm md:text-2xl leading-6">creating multiple
             streams of income</span> becomes imperative. There are remote online data
-            validation and search relevance jobs in your locale. <span className="text-orange-500 text-sm md:text-2xl leading-6">
+            validation and search relevance jobs in your locale. <span className="block text-orange-500 text-sm md:text-2xl leading-6">
             You get the
             job. We do the job. You get the cash you pay us. You pay us.
               </span>
           </p>
-          <div className="md:-ml-16  mt-6 md:mt-6 sm:mt-6 flex justify-center space-x-6 text-sm">
+          <div className="md:-ml-16  mt-6 md:mt-10 sm:mt-10 flex justify-center space-x-6 text-sm">
             <button
               type="button"
               class="  px-6 py-3 text-lg font-bold text-white bg-orange-500 hover:bg-orange-600 focus:ring-4  rounded-full focus:outline-none "
