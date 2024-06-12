4c3cb6afe09bd792ef66b5f60d8cbc86f6b36f4b
achimugu <sachiimugu@gmail.com>
2024-06-12 15:24:52 +0100

    h

--- components/Howwouldyo.jsx
+++ components/Howwouldyo.jsx
@@ -6,7 +6,7 @@ const Howwouldyo = () => {
     <div className="md:relative bg-sky-100  pb-4 md:flex justify-center items-center">
       <div className=" md:w-[70%]">
         <p className="md:text-[2.8125rem] text-[1.525rem] leading-normal md:leading-0 px-2 p  text-slate-900 font-[700] text-center md:py-8 md:px-8">
-          We Do The Job You Get The Cash
+          We do the Job You get the Cash
         </p>
         <p className="text-sky-950 md:text-2xl px-2 md:px-8 text-center ">
           This is an income opportunity that will not interfere with your time
