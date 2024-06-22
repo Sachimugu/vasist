d14bd207f1f3007120eb995f10dcc8b774ad9040
achimugu <sachiimugu@gmail.com>
2024-06-22 15:04:42 +0100

    jsjd

--- components/ContactLogos.jsx
+++ components/ContactLogos.jsx
@@ -1,22 +1,65 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const ContactLogos = () => {
-  return (
-    <div className='bg-sky-200'>
-        <p className="md:text-[3.125rem] text-[1.825rem] leading-9 md:leading-0 px-2 py-3  text-slate-900 font-[700] text-center md:py-8 md:px-16">
-          Contact Us
-          <br className="hidden md:block" />
-        </p>
-    <div className='flex gap-8 md:gap-16 justify-center item-center px-4 py-2 md:p-8 bg-sky-200 shadow-md '>
-        
-        <Image src='/images/gm.svg' width={90} height={90} alt='icons' className="cursor-pointer" />
-        <Image src='/images/wh.svg' width={100} height={100} alt='icons' className="cursor-pointer"/>
-        <Image src='/images/te.svg' width={100} height={100} alt='icons' className="cursor-pointer" />
-        <Image src='/images/li.svg' width={100} height={100} alt='icons' className="cursor-pointer" />
-    </div>
-    </div>
-  )
-}
+  const telegramLink = "https://t.me/@remotraters"; // Replace with your link
 
-export default ContactLogos
+  const whatsAppLink =
+    "https://wa.me/+14134727947?text=You%20are%20most%20warmly%20welcome%20to%20RemoteRaters%2C%20and%20we%20are%20glad%20to%20assist%20you%20with%20any%20queries%20you%20might%20have.%20Kindly%20proceed"; // Replace with your link
+
+  return (
+    <div className="bg-sky-200">
+      <p className="md:text-[3.125rem] text-[1.825rem] leading-9 md:leading-0 px-2 py-3  text-slate-900 font-[700] text-center md:py-8 md:px-16">
+        Contact Us
+        <br className="hidden md:block" />
+      </p>
+      <div className="flex gap-8 md:gap-16 justify-center item-center px-4 py-2 md:p-8 bg-sky-200 shadow-md ">
+        <Link
+          href="mailto:enquiries@remoteraters.com"
+          className="hover:shadow-lg px-4 flex justify-center items-center"
+        >
+          <Image
+            src="/images/gm.svg"
+            width={90}
+            height={90}
+            alt="Gmail Icon"
+            className="cursor-pointer" // Add your CSS class for pointer cursor
+          />
+        </Link>
+
+        <Link href={whatsAppLink}  className="hover:shadow-lg px-4 flex justify-center items-center">
+          <Image
+            src="/images/wh.svg"
+            width={100}
+            height={100}
+            alt="WhatsApp Icon"
+            className="cursor-pointer"
+          />
+        </Link>
+
+        <Link href={telegramLink}  className="hover:shadow-lg px-4 flex justify-center items-center">
+          <Image
+            src="/images/te.svg"
+            width={100}
+            height={100}
+            alt="Telegram Icon"
+            className="cursor-pointer"
+          />
+        </Link>
+
+        <Link href="https://www.linkedin.com/company/remoteraters/about/"  className="hover:shadow-lg px-4 flex justify-center items-center shadow-white">
+          <Image
+            src="/images/li.svg"
+            width={100}
+            height={100}
+            alt="LinkedIn Icon"
+            className="cursor-pointer"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ContactLogos;
--- components/FooterSection.jsx
+++ components/FooterSection.jsx
@@ -16,8 +16,8 @@ function FooterSection() {
           </p>
           <p className="text-sm text-slate-200 mt-[1.19rem] pb-2 text-center">
             We are a team of rating experts who work online and evaluate data
-             <span className="hidden text-center md:block"></span> providing quality checks and
-            judgements
+            <span className="hidden text-center md:block"></span> providing
+            quality checks and judgements
           </p>
         </div>
         <div className="flex gap-8 flex-grow justify-around">
@@ -25,7 +25,7 @@ function FooterSection() {
             <p className="text-slate-200 font-[700] text-lg md:text-[1.3125rem]">
               Company
             </p>
-            <div className="flex flex- gap-2">
+            <div className="flex flex-col gap-2">
               <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 md:font-[500]">
                 About
               </p>
@@ -41,15 +41,36 @@ function FooterSection() {
             <p className="text-slate-200 font-[700]  text-[1.3125rem]">
               Contact
             </p>
-            <div className="flex flex- gap-2">
-              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
-                Help
+            <div className="flex flex-col gap-2">
+              <p className="text-slate-400  flex gap-1 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
+                <Image
+                  src="/images/gm.svg"
+                  width={11}
+                  height={11}
+                  alt="Gmail Icon"
+                  className="cursor-pointer" // Add your CSS class for pointer cursor
+                />
+                Enquiries@remoteraters.com{" "}
               </p>
-              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
-                Press
+              <p className="text-slate-400 flex gap-1 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
+              <Image
+            src="/images/wh.svg"
+            width={15}
+            height={15}
+            alt="WhatsApp Icon"
+            className="cursor-pointer"
+          />
+                +14134727947
               </p>
-              <p className="text-slate-400 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
-                Affilates
+              <p className="text-slate-400 flex gap-1 text-sm text-[1.125rem] hover:text-orange-500 font-[500]">
+              <Image
+            src="/images/te.svg"
+            width={15}
+            height={15}
+            alt="Telegram Icon"
+            className="cursor-pointer"
+          />
+                @remoteraters
               </p>
             </div>
           </div>
